1d2c683fc65438be378a95c705f686408c4851bb
Marius <marijonass@gmail.com>
2023-08-23 19:19:30 +0300

    first commit

---
+++ src/main/java/com/marius/blogma2023/BlogMa2023Application.java
@@ -0,0 +1,13 @@
+package com.marius.blogma2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMa2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMa2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/marius/blogma2023/BlogMa2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marius.blogma2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMa2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
