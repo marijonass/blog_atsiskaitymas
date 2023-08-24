22ca8e9acf38b19151e388eab3f85eb672ea71da
Marius <marijonass@gmail.com>
2023-08-24 18:51:15 +0300

    db sarysis

---
+++ src/main/java/com/marius/blogma2023/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.marius.blogma2023.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name="topic_id", nullable=false)
+    private Topic topic;
+}
